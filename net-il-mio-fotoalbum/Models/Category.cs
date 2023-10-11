f145db8e0584ea9af36c8d3ab6948f13ee02b920
giovannitamburini <giovatambu@gmail.com>
2023-10-11 12:06:50 +0200

    added Photo model & Category model

---
+++ net-il-mio-fotoalbum/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace net_il_mio_fotoalbum.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        // relazione N:N con Photo
+        public List<Photo> Photos { get; set; }
+
+        public Category() { }
+    }
+}
---
+++ net-il-mio-fotoalbum/Models/Photo.cs
@@ -0,0 +1,23 @@
+﻿namespace net_il_mio_fotoalbum.Models
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public bool IsVisible { get; set; }
+
+        // relazione N:N con Category
+        public List<Category> Categories { get; set; }
+
+        public Photo() { }
+
+        public Photo(string title, string description, string image)
+        {
+            this.Title = title;
+            this.Description = description;
+            this.Image = image;
+        }
+    }
+}
