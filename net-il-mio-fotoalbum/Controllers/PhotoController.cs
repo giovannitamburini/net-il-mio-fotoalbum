389e631c98d63fb8a8236a3d07ffcc41022fc34b
giovannitamburini <giovatambu@gmail.com>
2023-10-11 21:47:02 +0200

    added basic Update

--- net-il-mio-fotoalbum/Controllers/PhotoController.cs
+++ net-il-mio-fotoalbum/Controllers/PhotoController.cs
@@ -66,5 +66,56 @@ namespace net_il_mio_fotoalbum.Controllers
                 return RedirectToAction("Index");
             }
         }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            using(PhotoPortfolioContext db = new PhotoPortfolioContext())
+            {
+                Photo? photoToEdit = db.Photos.Where(photo => photo.Id == id).FirstOrDefault();
+
+                if(photoToEdit == null)
+                {
+                    return NotFound("La foto che voui modificare non è stata trovata");
+                }
+                else
+                {
+                    return View("Update", photoToEdit);
+                }
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(int id,Photo ModifiedPhoto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Update", ModifiedPhoto);
+            }
+
+            using (PhotoPortfolioContext db = new PhotoPortfolioContext())
+            {
+                Photo photoToUpdate = db.Photos.Where(photo => photo.Id == id).FirstOrDefault();
+
+                if(photoToUpdate != null)
+                {
+                    photoToUpdate.Title = ModifiedPhoto.Title;
+                    photoToUpdate.Description = ModifiedPhoto.Description;
+                    photoToUpdate.PathImage = ModifiedPhoto.PathImage;
+
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return NotFound("Mi dispiace, la foto da aggiornare non è stata trovata");
+                }
+            }
+
+
+
+        }
     }
 }
