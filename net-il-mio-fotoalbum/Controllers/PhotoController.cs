4d9d447f0bc6ed76876bbd2cf3048d48d0fb9a8b
giovannitamburini <giovatambu@gmail.com>
2023-10-11 18:06:21 +0200

    added Admin Details

--- net-il-mio-fotoalbum/Controllers/PhotoController.cs
+++ net-il-mio-fotoalbum/Controllers/PhotoController.cs
@@ -10,11 +10,35 @@ namespace net_il_mio_fotoalbum.Controllers
         {
             using (PhotoPortfolioContext db = new PhotoPortfolioContext())
             {
-                List<Photo> photos = db.Photos.ToList();
+                try
+                {
+                    List<Photo> photos = db.Photos.ToList();
 
-                return View("Index", photos);
+                    return View("Index", photos);
+
+                }catch (Exception ex)
+                {
+                    return NotFound($"La lista delle foto è vuota \n" +
+                        $"Messaggio d'errore: {ex.Message}");
+                }
             }
+        }
 
+        public IActionResult Details(int id)
+        {
+            using (PhotoPortfolioContext db = new PhotoPortfolioContext())
+            {
+                Photo? foundedPhoto = db.Photos.Where(photo => photo.Id == id).FirstOrDefault();
+
+                if (foundedPhoto == null)
+                {
+                    return NotFound($"La foto con id '{id}' non è stata trovata");
+                }
+                else
+                {
+                    return View("Details", foundedPhoto);
+                }
+            }
         }
     }
 }
