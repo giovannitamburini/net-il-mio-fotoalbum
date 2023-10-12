a32fcc83cd573ead5239ab479edc65305b9496a8
giovannitamburini <giovatambu@gmail.com>
2023-10-12 16:31:56 +0200

    added message API controller

---
+++ net-il-mio-fotoalbum/Controllers/API/MessageAPIController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using net_il_mio_fotoalbum.Database;
+using net_il_mio_fotoalbum.Models;
+
+namespace net_il_mio_fotoalbum.Controllers.API
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MessageAPIController : ControllerBase
+    {
+        private PhotoPortfolioContext _MyDb;
+
+        public MessageAPIController(PhotoPortfolioContext mydb)
+        {
+            this._MyDb = mydb;
+        }
+
+
+        [HttpPost]
+        public IActionResult CreateMessage([FromBody] Message message)
+        {
+            try
+            {
+                _MyDb.Messages.Add(message);
+
+                _MyDb.SaveChanges();
+
+                return Ok("Messaggio inviato con successo");
+            }
+            catch(Exception ex)
+            {
+                return BadRequest("Mi dispiace ma qualcosa è andato storto\n" + $"Messaggio di errore: { ex.Message}");
+            }
+        }
+    }
+}
